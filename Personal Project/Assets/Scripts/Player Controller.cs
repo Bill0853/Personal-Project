0aa9ab2c469bd43130d709d7878fece9470de127
Bill0853 <121959980+Bill0853@users.noreply.github.com>
2023-03-20 11:38:04 -0400

    personal restart

--- Personal Project/Assets/Scripts/Player Controller.cs
+++ Personal Project/Assets/Scripts/Player Controller.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationCamera : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float rotationSpeed;
 
+    private Rigidbody playerRb;
+    private float verticalInput;
+    private float horizontalInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime); 
+        
     }
 }
